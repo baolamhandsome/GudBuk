421a98f2a62b3bc0fbcb30bc4cf1278aa974ed36
hopie <vuthihop06@gmail.com>
2026-06-19 07:45:39 +0700

    Fix tokenlogin (tokenid -> token)

--- app/models/tokenLogin.php
+++ app/models/tokenLogin.php
@@ -8,14 +8,14 @@ class tokenLogin extends Dbcore
     }
     public function getToken($token)
     {
-        $sql = "SELECT * FROM token_login WHERE tokenid = '$token' AND expires_at > NOW()";
+        $sql = "SELECT * FROM token_login WHERE token = '$token' AND expires_at > NOW()";
         return $this->getOne($sql);
     }
 
     // tạm thời sử dụng hàm ntn
     public function deleteToken($token)
     {
-        $sql = "DELETE FROM token_login WHERE tokenid = '$token'";
+        $sql = "DELETE FROM token_login WHERE token = '$token'";
         return $this->update($sql);
     }
 }
