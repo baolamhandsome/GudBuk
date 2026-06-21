bb419e6d4daea962b09700b9b64564956a9da921
Nguyen Huy <huy.ngndinh@gmail.com>
2026-06-21 17:22:26 +0700

    fix: fix bug where cart and order icon sometimes don't redirect as intended

--- app/views/fixed_components/header.php
+++ app/views/fixed_components/header.php
@@ -7,37 +7,38 @@
     <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <link rel="stylesheet" href="/GudBuk/public/css/header.css">
-	<script type="text/javascript" async src="/GudBuk/public/js/header.js"></script>
     <title>gudbuk</title>
 </head>
-<header class="header">
-    <a href="/gudbuk/home" class="logo">GudBuk</a>
+<body>
+	<header class="header">
+		<a href="/gudbuk/home" class="logo">GudBuk</a>
 
-    <form action="/gudbuk/search" method="GET" class="search-form">
-        <input type="search" name="query" id="search-box" placeholder="Find book by title, author...">
+		<form action="/gudbuk/search" method="GET" class="search-form">
+			<input type="search" name="query" id="search-box" placeholder="Find book by title, author...">
 
-        <button type="submit" id="search-submit-btn" style="background: none; border: none; cursor: pointer;">
-            <i class="fas fa-search"></i>
-        </button>
-    </form>
-    <div class="icons">
-        <a>
-            <div id="cart-btn" class="fas fa-shopping-cart"> </div>
-        </a>
+			<button type="submit" id="search-submit-btn" style="background: none; border: none; cursor: pointer;">
+				<i class="fas fa-search"></i>
+			</button>
+		</form>
+		<div class="icons">
+			<a>
+				<div id="cart-btn" class="fas fa-shopping-cart"> </div>
+			</a>
 
-		<a>
-			<div id="order-btn" class="fas fa-basket-shopping"> </div>
-		</a>
+			<a>
+				<div id="order-btn" class="fas fa-basket-shopping"> </div>
+			</a>
 
-        <a href="/gudbuk/profile">
-            <div id="user-btn" class="fas fa-user"></div>
-        </a>
+			<a href="/gudbuk/profile">
+				<div id="user-btn" class="fas fa-user"></div>
+			</a>
 
-        <a href="/gudbuk/logout">
-            <div id="logout-btn" class="fas fa-sign-out-alt"></div>
-        </a>
-    </div>
+			<a href="/gudbuk/logout">
+				<div id="logout-btn" class="fas fa-sign-out-alt"></div>
+			</a>
+		</div>
 
-</header>
-
+	</header>
+	<script type="text/javascript" src="/GudBuk/public/js/header.js"></script>
+</body>
 </html>
